c09a03ebff8d811ac1b4227a5563a9a79d3f66a7
Loki22784 <gudivadalokesh22124@gmail.com>
2026-06-20 00:47:32 +0530

    Enhance CORS configuration for server

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,18 @@ const {Server} = require("socket.io")
 const cors = require("cors")
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:5173","https://real-time-whiteboard-pink.vercel.app/"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
 const server = http.createServer(app)
 const io = new Server(server,{
     cors: {
-        origin: ["http://localhost:5173","https://real-time-whiteboard-pink.vercel.app/"]
+        origin: ["http://localhost:5173","https://real-time-whiteboard-pink.vercel.app/"],
+        methods: ["GET", "POST"],
+        credentials: true
     }
 })
 
